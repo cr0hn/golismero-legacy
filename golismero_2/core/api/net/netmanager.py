2d05a579e3dd8ad23440e3ad4a7beb8b9d726b73
cr0hn <garcia.garcia.daniel@gmail.com>
2013-02-13 16:27:10 +0100

    fix a semaphore

---
+++ golismero_2/core/api/net/netmanager.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+"""
+GoLismero 2.0 - The web knife - Copyright (C) 2011-2013
+
+Author: Daniel Garcia Garcia a.k.a cr0hn | dani@iniqua.com
+
+Golismero project site: http://code.google.com/p/golismero/
+Golismero project mail: golismero.project@gmail.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
+from core.main.commonstructures import Singleton
+
+#------------------------------------------------------------------------------
+class NetManager(Singleton):
+    """"""
+
+    TYPE_WEB = 0
+    TYPE_FTP = 0
+
+    #----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+
+        # For Singleton pattern
+        if self._is_instanced:
+            return
+
+
+
+    #----------------------------------------------------------------------
+    def get_connection(self, protocol = TYPE_WEB):
+        """
+        Get a connection of for an specific protocol.
+        """
+        pass
