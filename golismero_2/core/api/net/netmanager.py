7bfbbffdc3fe5201e902e29039064652f1d8a41e
cr0hn <garcia.garcia.daniel@gmail.com>
2013-02-26 12:40:05 +0100

    fixed exception catch

--- golismero_2/core/api/net/netmanager.py
+++ golismero_2/core/api/net/netmanager.py
@@ -210,53 +210,53 @@ class Web (Protocol):
         if request.is_cacheable and self._cache.is_cached(request):
             m_response = self._cache.get_cache(request)
         else:
+            #
             # Get URL
-            try:
-                # Set redirect options
-                request.follow_redirects = request.follow_redirects if request.follow_redirects else self.__follow_redirects
+            #
 
-                # timing init
-                t1 = time()
-                # Select request type
-                m_response = None
-                if "POST" == request.method or "PUT" == request.method:
-                    m_response = self.__http_pool_manager.request(
-                        method = request.method,
-                        url = request.url,
-                        redirect = request.follow_redirects,
-                        headers = request.raw_headers,
-                        fields = request.post_data,
-                        encode_multipart = request.files_attached
-                    )
-                elif "GET" == request.method:
-                    m_response = self.__http_pool_manager.request(
-                        method = request.method,
-                        url = request.url,
-                        redirect = request.follow_redirects,
-                        headers = request.raw_headers,
-                    )
-                else:
-                    m_response = self.__http_pool_manager.request(
-                        method = request.method,
-                        url = request.url,
-                        redirect = request.follow_redirects,
-                        headers = request.raw_headers,
-                        fields = request.post_data,
-                    )
+            # Set redirect options
+            request.follow_redirects = request.follow_redirects if request.follow_redirects else self.__follow_redirects
 
-                # timin end
-                t2 = time()
+            # timing init
+            t1 = time()
+            # Select request type
+            m_response = None
+            if "POST" == request.method or "PUT" == request.method:
+                m_response = self.__http_pool_manager.request(
+                    method = request.method,
+                    url = request.url,
+                    redirect = request.follow_redirects,
+                    headers = request.raw_headers,
+                    fields = request.post_data,
+                    encode_multipart = request.files_attached
+                )
+            elif "GET" == request.method:
+                m_response = self.__http_pool_manager.request(
+                    method = request.method,
+                    url = request.url,
+                    redirect = request.follow_redirects,
+                    headers = request.raw_headers,
+                )
+            else:
+                m_response = self.__http_pool_manager.request(
+                    method = request.method,
+                    url = request.url,
+                    redirect = request.follow_redirects,
+                    headers = request.raw_headers,
+                    fields = request.post_data,
+                )
 
-                # Calculate response time
-                m_time = t2 - t1
+            # timin end
+            t2 = time()
 
-                m_response = HTTP_Response(m_response, m_time, request)
+            # Calculate response time
+            m_time = t2 - t1
 
-                # Cache is enabled?
-                if request.is_cacheable:
-                    self._cache.set_cache(m_response)
-            except Exception, e:
-                Logger.log_error_verbose("Unknown error: '%s'." % e.message)
+            m_response = HTTP_Response(m_response, m_time, request)
+
+            # Cache is enabled?
+            if request.is_cacheable:
+                self._cache.set_cache(m_response)
 
         return m_response
 
