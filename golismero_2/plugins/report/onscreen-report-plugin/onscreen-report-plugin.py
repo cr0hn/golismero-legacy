4efb621b4d75a4474a1155a6afa09302fb0b6aa8
cr0hn <garcia.garcia.daniel@gmail.com>
2013-03-11 17:57:17 +0100

    Added new UI plugin: screen report plugin. The default report

---
+++ golismero_2/plugins/report/onscreen-report-plugin/onscreen-report-plugin.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+GoLismero 2.0 - The web knife - Copyright (C) 2011-2013
+
+Author: Daniel Garcia Garcia a.k.a cr0hn | dani@iniqua.com
+
+Golismero project site: http://code.google.com/p/golismero/
+Golismero project mail: golismero.project@gmail.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
+
+from core.api.plugin import ReportPlugin
+from core.api.results.result import Result
+from core.api.results.information.information import Information
+from collections import Counter
+from core.api.colorize import *
+
+
+class ScreenReport(ReportPlugin):
+    """
+    This plugin to display reports on screen
+    """
+
+    #----------------------------------------------------------------------
+    @property
+    def report_type(self):
+        """
+        Get an string with the report name that will be generate. For
+        example: text, html, grepable...
+
+        :returns: str -- type of report
+        """
+        return "screen"
+
+
+    #----------------------------------------------------------------------
+    def generate_report(self, config, results):
+        """
+        Run plugin and generate report.
+
+        :param config: configuration for report
+        :type config: GlobalParams
+
+        :param results: iterable with results.
+        :type results: iterable.
+        """
+
+        # Check for None
+        if not config or not results:
+            return
+
+        # All results, with not nulls
+        m_results = filter(lambda x: x, results)
+
+        m_counter = Counter()
+
+        # Header
+        print "\n\n--= %s =--" % colorize("Report", "cyan")
+
+        #
+        # 1 - Get urls
+        #
+        print "\n- %s - \n"% colorize("Spidered URLs", "yellow")
+
+        for u in filter(lambda x: x.result_type == Result.TYPE_INFORMATION and x.information_type == Information.INFORMATION_URL , m_results):
+            print "+ %s" % str(u)
+            m_counter['url'] += 1
+
+
+        #
+        # End - Write summary
+        #
+        print "\n- %s -\n" % colorize("Summary", "yellow")
+
+        # Urls
+        print "+ Total URLs: %s\n\n" % colorize(str(m_counter['url']), "yellow")
+
+
