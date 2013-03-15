5df1d9bb018de1884e034218f50b435a5a398cdc
Mario Vilas <mvilas@gmail.com>
2013-03-15 19:13:06 +0100

    bla

--- golismero_2/core/managers/uimanager.py
+++ golismero_2/core/managers/uimanager.py
@@ -24,6 +24,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
+__all__ = ["UIManager"]
+
 from .priscillapluginmanager import PriscillaPluginManager
 from ..messaging.notifier import UINotifier
 from ..messaging.message import Message
