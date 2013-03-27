feb5cedde9df13ee456b6286b20010736ec39aa1
cr0hn <garcia.garcia.daniel@gmail.com>
2013-03-27 17:32:18 +0100

    revised and fixed API to compare a pool of texts to search differences

--- golismero_2/golismero/api/text/matching_analyzer.py
+++ golismero_2/golismero/api/text/matching_analyzer.py
@@ -31,6 +31,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
 from golismero.api.logger import Logger
+import hashlib
+from difflib import SequenceMatcher
+from diff_match_patch import diff_match_patch
 
 
 #----------------------------------------------------------------------
@@ -87,8 +90,15 @@ class MatchingAnalyzerElement:
     """"""
 
     #----------------------------------------------------------------------
-    def __init__(self):
+    def __init__(self, attrs):
         """Constructor"""
+        self.__attrs = attrs
+
+    #----------------------------------------------------------------------
+    def __getattr__(self, name):
+        """"""
+        return self.__attrs[name]
+
 
 
 
@@ -96,13 +106,28 @@ class MatchingAnalyzerElement:
 
 #------------------------------------------------------------------------------
 class MatchingAnalyzer:
-    """"""
+    """
+    Text analyzer and comparer texts.
+
+    Compare an undetermined number of text from a base text and generates
+    an iterator with those that are sufficiently different.
+    """
 
     #----------------------------------------------------------------------
-    def __init__(self, base_text):
+    def __init__(self, base_text, matching_level = 0.52, deviation = 1.15):
         """Constructor"""
         self.__base_text = base_text
-        self.__text_lists = {}
+        self.__unique_strings = []
+        self.__new_data = False
+        self.__average_level = 0.0
+
+        # Results
+        self.__results_matching_level = {}
+        self.__results_other_args = {}
+
+        # Advanced values
+        self.__matching_level = matching_level
+        self.__deviation = deviation
 
 
     #----------------------------------------------------------------------
@@ -114,52 +139,87 @@ class MatchingAnalyzer:
 
 
     #----------------------------------------------------------------------
-    def append(self, key, text, **kargs):
-        """"""
-        if text:
-            #l_matching_level = get_matching_level(self.__base_text, text)
+    def append(self, text, **kargs):
+        """
+        If the matching level is accepted store it and the others params in
+        **kargs.
 
-            #if l_matching_level < 0.52:
+        :param text: text to compare.
+        :type text: str
+
+        :param **kargs: undefined numbre of params.
+        :type **kargs: special param.
+
+        :return: True if text is accepted. False otherwise.
+        :retype: bool
+        """
+        if text:
+            l_matching_level = get_matching_level(self.__base_text, text)
+
+            if l_matching_level < self.__matching_level:
+                # Set to new data received
+                self.__new_data = True
+
+                l_key = hashlib.md5(text).hexdigest()
+
                 # Append to partial results
-            #    self.__results.append(key, severity_vectors[m_name], l_matching_level)
-            pass
+                self.__results_matching_level[l_key] = l_matching_level
+                self.__results_other_args[l_key] = kargs
+
+                return True
+            else:
+                return False
 
 
     #----------------------------------------------------------------------
     @property
     def unique_texts(self):
-        """"""
+        """
+        Return an iterable with unique text.
+        """
+        if self.__new_data:
+            self.__calculate()
 
+        for ut in self.__unique_strings:
+            yield ut
 
+    #----------------------------------------------------------------------
+    @property
+    def level_average(self):
+        """
+        Average of maching level.
 
+        :rtype: float
+        """
+        if self.__new_data:
+            if self.__results_matching_level:
+                self.__average_level = sum(self.__results_matching_level.itervalues()) / len(self.__results_matching_level)
+            else:
+                self.__average_level = 0.0
 
+        return self.__average_level
 
+    #----------------------------------------------------------------------
+    def __calculate(self):
+        """
+        Calculate the elements that are really different:
 
-#    Logger.log_more_verbose("Bruteforcer - Discovered partial url: '%s'!!" % l_url)
+        Calculate the level of correpondence for all elements. We calculate the
+        deviation of 5%. All elements in of these deviation are part of same page of
+        error, and then skip it.
+        """
+        if self.level_average:
+            m_average = self.level_average
+            m_results_append = self.__unique_strings
 
-    # Send_ response, HTML and URL to kernel.
-    #self.send_info(Url(l_url))
-    #self.send_info(p)
-    #if not p.information:
-    #    self.send_info(p.information)
+            m_unique_strings_append = self.__unique_strings.append
 
+            for l_key, l_info in self.__results_matching_level.iteritems():
+                l_value = l_info # Original value
+                l_value_deviation = l_value * self.__deviation # 15% of deviation
 
-#    if m_store_info.average_level > 0:
-#        m_average = m_store_info.average_level
-#
-#        m_results_append = m_results.append
-#
-#        #for i, l_level in enumerate(m_discovered_level):
-#        for l_info in m_store_info:
-#            l_value = l_info.level # Original value
-#            l_value_deviation = l_value * 1.15 # 15% of deviation
-#
-#            # value < average < value * 5% => skip
-#            if not (l_value < m_average and m_average < l_value_deviation):
-#
-#                Logger.log_verbose("Bruteforcer - discovered URL: %s !!!" % m_discovered_url[i][0])
-#
-#                #
-#                # Send vulnerability
-#                #
-#                l_vuln = UrlDisclosure(l_info.url)
+                # NOT value < average < value * 5% => skip
+                if not (l_value < m_average and m_average < l_value_deviation):
+                    m_unique_strings_append(MatchingAnalyzerElement(self.__results_other_args[l_key]))
+
+            self.__new_data = False
