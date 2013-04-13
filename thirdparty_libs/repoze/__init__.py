ad6663d7210914d7d860c19ea5b9c057eb70bdc6
Mario Vilas <mvilas@gmail.com>
2013-04-13 16:03:59 +0200

    Updated repoze.lru

--- thirdparty_libs/repoze/__init__.py
+++ thirdparty_libs/repoze/__init__.py
@@ -1,4 +1,4 @@
 try:
-	__import__('pkg_resources').declare_namespace(__name__)
+    __import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
-	pass
+    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
