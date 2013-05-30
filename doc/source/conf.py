a2085e03615ade303be1f4bc99586e15c3995183
cr0hn <cr0hn@cr0hn.com>
2013-05-30 13:28:56 +0200

    modified config for use relative paths

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,9 +16,9 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.append(os.path.abspath('/Users/Dani/Documents/Projects/Golismero_2.0/src_github/doc/source/'))
-sys.path.append(os.path.abspath('/Users/Dani/Documents/Projects/Golismero_2.0/src_github/'))
-sys.path.append(os.path.abspath('/Users/Dani/Documents/Projects/Golismero_2.0/src_github/thirdparty_libs'))
+
+sys.path.append(os.path.abspath(".."))
+sys.path.append(os.path.abspath(os.path.join("..", "thirdparty_libs")))
 
 # -- General configuration -----------------------------------------------------
 
