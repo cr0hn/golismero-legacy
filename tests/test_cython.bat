5186a9fd3375297b30afec1edc322be66bea1ced
Mario Vilas <mvilas@gmail.com>
2013-05-31 19:24:29 +0200

    Some improvements to the cython test.

--- tests/test_cython.bat
+++ tests/test_cython.bat
@@ -1,4 +1,5 @@
 @echo off
+del cython.log > nul 2> nul
 cd ..\golismero
 dir /b /s *.py > ..\_tmp.txt
 cd ..\plugins
@@ -6,7 +7,7 @@ dir /b /s *.py >> ..\_tmp.txt
 cd ..
 del /s *.pyc > nul 2> nul
 del /s *.pyo > nul 2> nul
-for /F "tokens=*" %%A in (_tmp.txt) do C:\Python27\python.exe C:\Python27\Scripts\cython.py "%%A"
+for /F "tokens=*" %%A in (_tmp.txt) do C:\Python27\python.exe C:\Python27\Scripts\cython.py "%%A" 2>> tests\cython.log
 del _tmp.txt
 del /s *.c > nul 2> nul
 cd tests
