db6b2e664231c68ff6c8d733feb59249275627a7
cr0hn <cr0hn@cr0hn.com>
2013-07-02 13:10:42 +0200

    Adapted the plugin to the new format and the new features for fingerprinting

--- golismero/api/data/information/webserver_fingerprint.py
+++ golismero/api/data/information/webserver_fingerprint.py
@@ -50,9 +50,9 @@ class WebServerFingerprint(Information):
 
 
     #----------------------------------------------------------------------
-    def __init__(self, name, version, banner, others = None):
+    def __init__(self, name, version, banner, canonical_name, related = None, others = None):
         """
-        :param name: Web server name. Example: "Apache"
+        :param name: Web server name as raw format. The name is taken from a raw banner with internal filters, and It can has errors with unusual servers. If you want to ensure that this is correct, use name_canonical instead. I.E: "Ricoh Aficio 1045 5.23 Web-Server 3.0" -> name = "Ricoh Aficio 1045 5.23 Web-Server"
         :type name: str
 
         :param version: Web server sersion. Example: "2.4"
@@ -60,6 +60,12 @@ class WebServerFingerprint(Information):
 
         :param banner: Complete description for web server. Example: "Apache 2.2.23 ((Unix) mod_ssl/2.2.23 OpenSSL/1.0.1e-fips)"
         :type banner: str
+
+        :param canonical_name: Web server name, at lowcase. The name will be one of the the file: 'wordlist/fingerprint/webservers_keywords.txt'. Example: "Apache"
+        :type canonical_name: str
+
+        :param related: dict with related webservers.
+        :type related: dict
 
         :param others: Map of other possible web servers by name and their probabilities of being correct [0.0 ~ 1.0].
         :type others: dict( str -> float )
@@ -73,6 +79,16 @@ class WebServerFingerprint(Information):
             raise TypeError("Expected str, got %s instead" % type(version))
         if not isinstance(banner, str):
             raise TypeError("Expected str, got %s instead" % type(banner))
+        if not isinstance(canonical_name, str):
+            raise TypeError("Expected str, got %s instead" % type(canonical_name))
+
+        if related is not None:
+            if not isinstance(others, dict):
+                raise TypeError("Expected dict, got %s instead" % type(others))
+            for v in related:
+                if not isinstance(v, str):
+                    raise TypeError("Expected str, got %s instead" % type(k))
+
         if others is not None:
             if not isinstance(others, dict):
                 raise TypeError("Expected dict, got %s instead" % type(others))
@@ -83,16 +99,22 @@ class WebServerFingerprint(Information):
                     raise TypeError("Expected float, got %s instead" % type(v))
 
         # Web server name.
-        self.__name    = name
+        self.__name           = name
 
         # Web server version.
-        self.__version = version
+        self.__version        = version
 
         # Web server banner.
-        self.__banner  = banner
+        self.__banner         = banner
+
+        # Web server canonical name
+        self.__name_canonical = canonical_name
 
         # Other possibilities for this web server.
-        self.__others  = others
+        self.__others         = others
+
+        # Related web servers
+        self.__related        = related
 
         # Parent constructor.
         super(WebServerFingerprint, self).__init__()
@@ -105,6 +127,16 @@ class WebServerFingerprint(Information):
             self.__version,
             self.__banner,
         )
+
+    #----------------------------------------------------------------------
+    @identity
+    def name_canonical(self):
+        """
+        :return: Web server name, at lowcase. The name will be one of the the file: 'wordlist/fingerprint/webservers_keywords.txt'. Example: "apache"
+        :rtype: str
+        """
+        return self.__name_canonical
+
 
 
     #----------------------------------------------------------------------
@@ -145,3 +177,11 @@ class WebServerFingerprint(Information):
         :rtype: dict( str -> float )
         """
         return self.__others
+
+    @identity
+    def related(self):
+        """
+        :return: Dict with the web server that act same as the discovered web server.
+        :rtype: dict(str -> set() ) -> ('iis' : ('hyperion'))
+        """
+        return self.__related
