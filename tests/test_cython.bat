74c6df02e3c0e9e19d41a5a67b5af0860bb0fcc2
Mario Vilas <mvilas@gmail.com>
2013-07-15 17:50:04 +0200

    Small changes.

--- tests/test_cython.bat
+++ tests/test_cython.bat
@@ -5,9 +5,12 @@ dir /b /s *.py > ..\_tmp.txt
 cd ..\plugins
 dir /b /s *.py >> ..\_tmp.txt
 cd ..
+del /s *.c > nul 2> nul
 del /s *.pyc > nul 2> nul
 del /s *.pyo > nul 2> nul
-for /F "tokens=*" %%A in (_tmp.txt) do C:\Python27\python.exe C:\Python27\Scripts\cython.py "%%A" 2>> tests\cython.log
+for /F "tokens=*" %%A in (_tmp.txt) do (
+    C:\Python27\python.exe C:\Python27\Scripts\cython.py "%%A" 2>> tests\cython.log
+)
 del _tmp.txt
 del /s *.c > nul 2> nul
 cd tests
