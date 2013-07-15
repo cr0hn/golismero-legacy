b2b0319b260007819a5b7745000338cfebb98361
Mario Vilas <mvilas@gmail.com>
2013-07-15 17:01:21 +0200

    Improved test.bat

--- tests/test.bat
+++ tests/test.bat
@@ -1,13 +1,14 @@
 @echo off
+rem Run all tests in the current folder.
 del *.log
 del *.sql
-call test_cython.bat
-call test_pyflakes.bat
-python test_pylint.py
-rem python test_pychecker.py > test-out.log 2> test-err.log
-python test_data.py >> test-out.log 2>> test-err.log
-python test_url.py >> test-out.log 2>> test-err.log
-python test_http_parser.py >> test-out.log 2>> test-err.log
-python test_auditdb.py >> test-out.log 2>> test-err.log
-python test_cachedb.py >> test-out.log 2>> test-err.log
-python test_parallel.py >> test-out.log 2>> test-err.log
+for /r %%i in (test_*.bat) do (
+    echo %%~ni
+    call %%i >> test-out.log 2>> test-err.log
+)
+for /r %%i in (test_*.py) do (
+    echo %%~ni
+    python %%i >> test-out.log 2>> test-err.log
+)
+echo test.py
+python test.py >> test-out.log 2>> test-err.log
