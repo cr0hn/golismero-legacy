7ee4dd17b7adc4cd45bffea139f99d23ddda80b8
Mario Vilas <mvilas@gmail.com>
2013-07-17 15:18:09 +0200

    Fixed indentation.

--- tests/test_dns.py
+++ tests/test_dns.py
@@ -1,43 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+GoLismero 2.0 - The web knife - Copyright (C) 2011-2013
+
+Authors:
+  Daniel Garcia Garcia a.k.a cr0hn | cr0hn<@>cr0hn.com
+  Mario Vilas | mvilas<@>gmail.com
+
+Golismero project site: https://github.com/cr0hn/golismero/
+Golismero project mail: golismero.project<@>gmail.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
+
 # Fix the module path for the tests.
 import sys
 import os
 from os import path
 try:
-	_FIXED_PATH_
+    _FIXED_PATH_
 except NameError:
-	here = path.split(path.abspath(__file__))[0]
-	if not here:  # if it fails use cwd instead
-		here = path.abspath(os.getcwd())
-	golismero = path.join(here, "..")
-	thirdparty_libs = path.join(golismero, "thirdparty_libs")
-	if path.exists(thirdparty_libs):
-		sys.path.insert(0, thirdparty_libs)
-		sys.path.insert(0, golismero)
-	_FIXED_PATH_ = True
+    here = path.split(path.abspath(__file__))[0]
+    if not here:  # if it fails use cwd instead
+        here = path.abspath(os.getcwd())
+    golismero = path.join(here, "..")
+    thirdparty_libs = path.join(golismero, "thirdparty_libs")
+    if path.exists(thirdparty_libs):
+        sys.path.insert(0, thirdparty_libs)
+        sys.path.insert(0, golismero)
+    _FIXED_PATH_ = True
 
 
 # Imports
 from golismero.api.net.dns import *
 from golismero.api.data.information.dns import *
 
+
 def test_registers():
 
-	d = Dns()
-	cname = d.resolve("www.terra.es", "CNAME")
+    d = DNS()
+    cname = d.resolve("www.terra.es", "CNAME")
 
-	for c in cname:
-		t = d.get_ips(c)
-		if c.type == "CNAME":
-			print c.target
+    for c in cname:
+        t = d.get_ips(c)
+        if c.type == "CNAME":
+            print c.target
 
-		for kk in t:
-			print kk.address
+        for kk in t:
+            print kk.address
 
-	a = d.get_aaaa("www.google.es")
-	for t in a:
-		if t.type == "AAAA":
-			print t.address
+    a = d.get_aaaa("www.google.es")
+    for t in a:
+        if t.type == "AAAA":
+            print t.address
 
 
 if __name__ == "__main__":
-	test_registers()
+    test_registers()
