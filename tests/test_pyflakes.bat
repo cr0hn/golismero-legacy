4b18cb0c3c7ff12ac46bf994b5fc8525a04bda42
Mario Vilas <mvilas@gmail.com>
2013-07-25 11:14:17 +0200

    This makes a lot more sens.

--- tests/test_pyflakes.bat
+++ tests/test_pyflakes.bat
@@ -1,8 +1,6 @@
 @echo off
 del pyflakes-out.log > nul 2> nul
 del pyflakes-err.log > nul 2> nul
-del pep8-out.log > nul 2> nul
-del pep8-err.log > nul 2> nul
 cd ..\golismero
 dir /b /s *.py > ..\_tmp.txt
 cd ..\plugins
@@ -10,20 +8,6 @@ dir /b /s *.py >> ..\_tmp.txt
 cd ..
 del /s *.pyc > nul 2> nul
 del /s *.pyo > nul 2> nul
-if exist C:\Python27\Scripts\pyflakes-script.py goto use_pyflakes
-if exist C:\Python27\Scripts\pep8-script.py goto use_pep8
-echo Neither PyFlakes nor PEP8 were found!
-goto end
-
-:use_pyflakes
 for /F "tokens=*" %%A in (_tmp.txt) do C:\Python27\python.exe C:\Python27\Scripts\pyflakes-script.py "%%A" >> tests\pyflakes-out.log 2>> tests\pyflakes-err.log
-if exist C:\Python27\Scripts\pep8-script.py goto use_pep8
-goto end
-
-:use_pep8
-for /F "tokens=*" %%A in (_tmp.txt) do C:\Python27\python.exe C:\Python27\Scripts\pep8-script.py "%%A" >> tests\pep8-out.log 2>> tests\pep8-err.log
-goto end
-
-:end
 del _tmp.txt
 cd tests
