7ca2686c8c6c1b97aba8d3db1f100f9223cb52f8
Mario Vilas <mvilas@gmail.com>
2013-07-29 23:26:29 +0200

    Small changes to the pmap test.

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -49,7 +49,7 @@ except NameError:
 from golismero.api.parallel import *
 
 from thread import get_ident
-from threading import enumerate
+from threading import enumerate as thread_list
 from random import randint
 from time import sleep, time
 
@@ -57,7 +57,7 @@ from time import sleep, time
 # Tests the parallel execution with random data.
 def test_pmap():
     print "Testing parallel execution with random data"
-    t_list = enumerate()
+    t_list = thread_list()
     func = lambda x: x
     for i in xrange(20):
         input_data   = [randint(-10, 10) for x in xrange(randint(10, 200))]
@@ -65,13 +65,13 @@ def test_pmap():
         control_data = map(func, input_data)
         assert output_data == control_data
         assert input_data == output_data
-    assert t_list == enumerate()
+    assert t_list == thread_list()
 
 
 # Tests the parallel execution with random data and multiple parameters.
 def test_pmap_multi():
     print "Testing parallel execution with random data and multiple parameters"
-    t_list = enumerate()
+    t_list = thread_list()
     def func(*args):
         return sum( x for x in args if x is not None )
     for i in xrange(10):
@@ -82,13 +82,13 @@ def test_pmap_multi():
         output_data  = pmap(func, *input_data, pool_size=10)
         control_data = map(func, *input_data)
         assert output_data == control_data
-    assert t_list == enumerate()
+    assert t_list == thread_list()
 
 
 # Tests the parallel execution with errors.
 def test_pmap_errors():
     print "Testing parallel execution with errors"
-    t_list = enumerate()
+    t_list = thread_list()
     def func(x):
         if x & 1:
             raise ValueError("I don't like odd numbers!")
@@ -100,13 +100,13 @@ def test_pmap_errors():
     output_data  = pmap(func,   xrange(200))
     control_data = map(control, xrange(200))
     assert output_data == control_data
-    assert t_list == enumerate()
+    assert t_list == thread_list()
 
 
 # Tests the parallel execution with delays.
 def test_pmap_delays():
     print "Testing parallel execution with delays"
-    t_list = enumerate()
+    t_list = thread_list()
     def func(x):
         ##print "(%d)" % get_ident()
         sleep(1)
@@ -116,27 +116,19 @@ def test_pmap_delays():
     test(1)
     test(4)
     test(8)
-    assert t_list == enumerate()
+    assert t_list == thread_list()
 
-# Tests the parallel with long data input
-def test_long_data_input():
-    print "Testing parallel with long data input"
 
-    def test(x):
-        return x
-
-    t1 = time()
-    pmap(test, range(1000))
-    t2 = time()
-
-    print "Ended in %.2f seconds" % (t2 - t1)
-
+# Tests the parallel execution with large inputs.
+def test_pmap_large_input():
+    print "Testing parallel execution with large inputs"
+    pmap(lambda x: x, range(1000))
 
 
 # Run all tests from the command line.
 if __name__ == "__main__":
-    #test_pmap()
-    #test_pmap_multi()
-    #test_pmap_errors()
-    #test_pmap_delays()
-    test_long_data_input()
+    test_pmap()
+    test_pmap_multi()
+    test_pmap_errors()
+    test_pmap_delays()
+    test_pmap_large_input()
