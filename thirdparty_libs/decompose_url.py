e21c12c34773df1291397ff08b7d4fac8dd81251
cr0hn <cr0hn@cr0hn.com>
2013-08-05 16:52:15 +0200

    added Mario Vilas to the header

--- thirdparty_libs/decompose_url.py
+++ thirdparty_libs/decompose_url.py
@@ -4,7 +4,9 @@
 """
 GoLismero 2.0 - The web knife - Copyright (C) 2011-2013
 
-Author: Daniel Garcia Garcia a.k.a cr0hn | cr0hn<@>cr0hn.com
+Authors:
+  Daniel Garcia Garcia a.k.a cr0hn | cr0hn<@>cr0hn.com
+  Mario Vilas | mvilas<@>gmail.com
 
 Golismero project site: https://github.com/cr0hn/golismero/
 Golismero project mail: golismero.project<@>gmail.com
