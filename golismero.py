cc99839bbc7305a95634c461c3206ba04c767dbc
Mario Vilas <mvilas@gmail.com>
2013-08-08 12:05:49 +0200

    Script to run GoLismero as a daemon. Untested.

--- golismero.py
+++ golismero.py
@@ -27,7 +27,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
-__all__ = ["launcher", "show_banner"]
+__all__ = []
 
 
 #----------------------------------------------------------------------
