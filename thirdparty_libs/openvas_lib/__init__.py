4c06465d07236b799839c322de2d42b955a7596a
Mario Vilas <mvilas@gmail.com>
2013-08-31 18:28:58 +0200

    Updated openvas_lib.

--- thirdparty_libs/openvas_lib/__init__.py
+++ thirdparty_libs/openvas_lib/__init__.py
@@ -9,15 +9,7 @@ because the official library doesn't work with OMP v4.0.
 """
 
 __license__ = """
-GoLismero 2.0 - The web knife - Copyright (C) 2011-2013
-
-Authors:
-  Daniel Garcia Garcia a.k.a cr0hn | cr0hn<@>cr0hn.com
-  Mario Vilas | mvilas<@>gmail.com
-
-Golismero project site: http://golismero-project.com
-Golismero project mail: golismero.project<@>gmail.com
-
+OpenVAS connector for OMPv4.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
