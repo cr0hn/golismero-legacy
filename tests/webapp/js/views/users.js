52c80711490953048b153721092f175213390543
rrequero <rrequero@buguroo.com>
2013-09-04 15:26:22 +0200

    changes in users view

--- tests/webapp/js/views/users.js
+++ tests/webapp/js/views/users.js
@@ -3,11 +3,27 @@ window.UsersView = Backbone.View.extend({
     className: '',
     initialize: function () {
         this.render();
+		this.model.bind('change', this.render, this);
+        this.model.bind('remove', this.remove, this);
     },
 
     render: function () {
 		$(this.el).html(this.template({list:this.model.toJSON()}));
         return this;
-    }
+    },
+	
+	editUser: function(id) {
+		alert("editUser " + id);
+	},
+	
+	createUser: function() {
+		alert("new User");
+	},
+	
+	removeUser: function(id) {
+		user = this.model.get(id);
+		user.set({name:'pepe'});
+	}
+	
 
 });
