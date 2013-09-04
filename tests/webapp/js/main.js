7e0d9d06f6e131c59a0a887df5f53050cb6e3a31
rrequero <rareq1987@gmail.com>
2013-09-04 22:50:08 +0200

    insert translate en/es and user form

--- tests/webapp/js/main.js
+++ tests/webapp/js/main.js
@@ -21,6 +21,7 @@ var AppRouter = Backbone.Router.extend({
             this.homeView = new HomeView();
         }
         $('#principal').html(this.homeView.el);
+		$('#principal').i18n();
         this.sidebarView.selectMenuItem('home-menu');
     },
 	users: function (id) {	
@@ -29,10 +30,12 @@ var AppRouter = Backbone.Router.extend({
 			userList.fetch({success: function(){
 				this.usersView = new UsersView({model:userList});
 				$('#principal').html(this.usersView.el);
+				$('#principal').i18n();
 			},error:function(p, error){
 				alert("Error " + p);
-				}});
-            //this.usersView = new UsersView({model:userList});			
+				}
+			});
+            		
         }else{
 			$('#principal').html(this.usersView.el);
 		}
@@ -46,4 +49,8 @@ var AppRouter = Backbone.Router.extend({
 utils.loadTemplate(['SidebarView', 'HomeView', 'UsersView'], function() {
     app = new AppRouter();
     Backbone.history.start();
+	i18n.init(function(t) {
+		$("body").i18n();	 
+	});
+	 
 });
--- tests/webapp/js/views/users.js
+++ tests/webapp/js/views/users.js
@@ -13,17 +13,28 @@ window.UsersView = Backbone.View.extend({
     },
 	
 	editUser: function(id) {
-		alert("editUser " + id);
+		//mostramos el formulario y cargamos los datos
+		$("#form-user").removeClass("hide").addClass("show");
+		$("#form-user legend:first").html(i18n.t("user.form.editUser"));
+		$("#form-user > .close").click(function(){$("#form-user").addClass("hide").removeClass("show");})
 	},
 	
 	createUser: function() {
-		alert("new User");
+		//reseteamos el formulario
+		$("#form-user").removeClass("hide").addClass("show");
+		$("#form-user legend:first").html(i18n.t("user.form.createUser"));
+		$("#form-user > .close").click(function(){$("#form-user").addClass("hide").removeClass("show");})
 	},
 	
 	removeUser: function(id) {
 		user = this.model.get(id);
 		user.set({name:'pepe'});
-	}
+	},
+	
+	resetForm: function(){
+		
+	}	
 	
 
 });
+
