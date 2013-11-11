fb1a38a77554168d195d7cf296579df8877dc71f
cr0hn <cr0hn@cr0hn.com>
2013-11-11 22:42:30 +0100

    simplied manage.py to original django command

--- gui/web/manage.py
+++ gui/web/manage.py
@@ -38,8 +38,4 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
     from django.conf import settings
 
-    proxy = xmlrpclib.ServerProxy("http://127.0.0.1:9000")
-
-    settings.RPC = proxy
-
     execute_from_command_line(sys.argv)
