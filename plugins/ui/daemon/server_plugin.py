3f2fc63621bf88bdca3637595d8eeb1d262b9aef
Mario Vilas <mvilas@gmail.com>
2013-11-17 13:58:55 -0300

    minor change

--- plugins/ui/daemon/server_plugin.py
+++ plugins/ui/daemon/server_plugin.py
@@ -901,13 +901,11 @@ class WebUIPlugin(UIPlugin):
 
             else:
                 # XXX TODO open the database manually here
-                #raise NotImplementedError(
-                    #"Querying finished audits is not implemented yet!")
-                return None
-        except:
-            return None
+                raise NotImplementedError(
+                    "Querying finished audits is not implemented yet!")
 
-
+        except Exception:
+            Logger.log_error(traceback.format_exc())
 
 
     #----------------------------------------------------------------------
