9ba4efb936e385d9f7eb1884f06d4a57043063f4
Mario Vilas <mvilas@gmail.com>
2013-11-29 13:07:29 +0100

    Detect sslscan-win bug: https://code.google.com/p/sslscan-win/issues/detail?id=2

--- plugins/testing/scan/sslscan.py
+++ plugins/testing/scan/sslscan.py
@@ -43,7 +43,7 @@ from golismero.api.logger import Logger
 from golismero.api.net import ConnectionSlot
 from golismero.api.plugin import ImportPlugin, TestingPlugin
 
-from os.path import join, sep
+from os.path import join, split, sep
 from traceback import format_exc
 from time import time
 
@@ -98,6 +98,76 @@ class SSLScanPlugin(TestingPlugin):
                 url = "http://sourceforge.net/projects/sslscan/"
             raise RuntimeError(
                 "SSLScan not found! You can download it from: %s" % url)
+
+        # Detect sslscan-win bug #2:
+        # https://code.google.com/p/sslscan-win/issues/detail?id=2
+        if sep == "\\":
+            from ctypes import windll, c_char_p, c_uint32, c_void_p, byref, create_string_buffer, Structure, sizeof, POINTER
+            class VS_FIXEDFILEINFO (Structure):
+                _fields_ = [
+                    ("dwSignature",         c_uint32),     # 0xFEEF04BD
+                    ("dwStrucVersion",      c_uint32),
+                    ("dwFileVersionMS",     c_uint32),
+                    ("dwFileVersionLS",     c_uint32),
+                    ("dwProductVersionMS",  c_uint32),
+                    ("dwProductVersionLS",  c_uint32),
+                    ("dwFileFlagsMask",     c_uint32),
+                    ("dwFileFlags",         c_uint32),
+                    ("dwFileOS",            c_uint32),
+                    ("dwFileType",          c_uint32),
+                    ("dwFileSubtype",       c_uint32),
+                    ("dwFileDateMS",        c_uint32),
+                    ("dwFileDateLS",        c_uint32),
+                ]
+            def GetFileVersionInfo(lptstrFilename):
+                _GetFileVersionInfoA = windll.version.GetFileVersionInfoA
+                _GetFileVersionInfoA.argtypes = [c_char_p, c_uint32, c_uint32, c_void_p]
+                _GetFileVersionInfoA.restype  = bool
+                _GetFileVersionInfoSizeA = windll.version.GetFileVersionInfoSizeA
+                _GetFileVersionInfoSizeA.argtypes = [c_char_p, c_void_p]
+                _GetFileVersionInfoSizeA.restype  = c_uint32
+                _VerQueryValueA = windll.version.VerQueryValueA
+                _VerQueryValueA.argtypes = [c_void_p, c_char_p, c_void_p, POINTER(c_uint32)]
+                _VerQueryValueA.restype  = bool
+                dwLen = _GetFileVersionInfoSizeA(lptstrFilename, None)
+                if dwLen:
+                    lpData = create_string_buffer(dwLen)
+                    if _GetFileVersionInfoA(lptstrFilename, 0, dwLen, byref(lpData)):
+                        lpFileInfo = POINTER(VS_FIXEDFILEINFO)()
+                        uLen = c_uint32(sizeof(lpFileInfo))
+                        if _VerQueryValueA(lpData, "\\", byref(lpFileInfo), byref(uLen)):
+                            sFileInfo = lpFileInfo.contents
+                            if sFileInfo.dwSignature == 0xFEEF04BD:
+                                return sFileInfo
+            def LOWORD(x):
+                return x & 0xFFFF
+            def HIWORD(x):
+                return (x >> 16) & 0xFFFF
+            filename = find_binary_in_path("sslscan")[0]
+            filename = split(filename)[0]
+            filename = join(filename, "libeay32.dll")
+            vinfo = GetFileVersionInfo(filename)
+            if not vinfo:
+                return # skip check if no file version info present
+            ms = vinfo.dwFileVersionMS
+            ls = vinfo.dwFileVersionLS
+            a = HIWORD(ms)
+            b = LOWORD(ms)
+            c = HIWORD(ls)
+            d = LOWORD(ls)
+            if not (
+                a > 0 or
+                b > 9 or
+                c > 8 or
+                d >= 20
+            ):
+                raise RuntimeError(
+                    "This version of OpenSSL (%s.%s.%s.%s) has a bug on"
+                    " Windows that causes a crash when run from GoLismero,"
+                    " please replace it with a newer version from: "
+                    "https://slproweb.com/products/Win32OpenSSL.html"
+                    % (a, b, c, d)
+                )
 
 
     #--------------------------------------------------------------------------
