3b6a59d51c9ef15cf675305ee0a7c80d478ba7ab
Mario Vilas <mvilas@gmail.com>
2013-12-03 10:58:32 +0100

    Roll back 85a14cb2e5a4eb971193fbd3d82daff57f35c150

--- plugins/testing/scan/sslscan.py
+++ plugins/testing/scan/sslscan.py
@@ -286,15 +286,12 @@ class SSLScanPlugin(TestingPlugin):
                         for c in t.findall(".//cipher")
                         if c.get("status") == "accepted"
                     ]
-                    try:
-                        # Get CERT dates.
-                        m_valid_before      = re.search("([a-zA-Z:0-9\s]+)( GMT)", t.find(".//not-valid-before").text).group(1)
-                        m_valid_after       = re.search("([a-zA-Z:0-9\s]+)( GMT)", t.find(".//not-valid-after").text).group(1)
-                        m_valid_before_date = datetime.strptime(m_valid_before, "%b %d %H:%M:%S %Y")
-                        m_valid_after_date  = datetime.strptime(m_valid_after, "%b %d %H:%M:%S %Y")
-                    except AttributeError:
-                        Logger.log("Pattern not found for certificate.")
-                        return None, 0
+
+                    # Get CERT dates.
+                    m_valid_before      = re.search("([a-zA-Z:0-9\s]+)( GMT)", t.find(".//not-valid-before").text).group(1)
+                    m_valid_after       = re.search("([a-zA-Z:0-9\s]+)( GMT)", t.find(".//not-valid-after").text).group(1)
+                    m_valid_before_date = datetime.strptime(m_valid_before, "%b %d %H:%M:%S %Y")
+                    m_valid_after_date  = datetime.strptime(m_valid_after, "%b %d %H:%M:%S %Y")
 
                     # Get subject.
                     m_cn                = re.search("(CN=)([0-9a-zA-Z\.\*]+)", t.find(".//subject").text).group(2)
