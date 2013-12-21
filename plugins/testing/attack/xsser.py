1be42941a4b6a6cd3e8d7d311ca6ec3a3a4c5d23
cr0hn <cr0hn@cr0hn.com>
2013-12-21 17:20:51 +0100

    - Xsser has a "peculiar" way to retreive GET params that will be tested. So, I had to redesigned the way that we can test each params. For each param, we must build an special query and call to the xsser. - Redesigned and simplified how to find xsser binary. - Added support for custom command line options for xsser. - Added more effective injection options by default. - Added support for web server params detection, and skip them.

--- plugins/testing/attack/xsser.py
+++ plugins/testing/attack/xsser.py
@@ -28,17 +28,19 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
+import shlex
+from time import time
+from traceback import format_exc
+
+from golismero.api.config import Config
 from golismero.api.data.resource.url import Url
 from golismero.api.data.vulnerability.injection.xss import XSS
 from golismero.api.external import run_external_tool, tempfile, \
-     find_binary_in_path, get_tools_folder
+    find_binary_in_path
 from golismero.api.logger import Logger
 from golismero.api.net import ConnectionSlot
+from golismero.api.net.web_utils import WEB_SERVERS_VARS
 from golismero.api.plugin import TestingPlugin
-
-from os.path import join, dirname, abspath, isfile
-from time import time
-from traceback import format_exc
 
 try:
     from xml.etree import cElementTree as ET
@@ -49,52 +51,90 @@ except ImportError:
 #------------------------------------------------------------------------------
 class XSSerPlugin(TestingPlugin):
 
-
     #--------------------------------------------------------------------------
     def check_params(self):
-        if not isfile( self.get_xsser() ):
+        if not find_binary_in_path("xsser.py"):
             raise RuntimeError(
                 "XSSer not found! You can download it from: "
                 "http://xsser.sourceforge.net/")
-
 
     #--------------------------------------------------------------------------
     def get_accepted_info(self):
         return [Url]
 
-
     #--------------------------------------------------------------------------
     def recv_info(self, info):
+
+        if not isinstance(info, Url):
+            return
 
         if not info.has_url_params and not info.has_post_params:
             Logger.log_more_verbose("URL '%s' has no parameters" % info.url)
             return
 
-        xsser_script = self.get_xsser()
+        # Get user args
+        user_args = shlex.split(Config.plugin_args.get("args", []))
 
+        # Result info
         results = []
 
         with tempfile(prefix="tmpxss", suffix=".xml") as filename:
 
             args = [
-                "-u",
-                info.url,
                 "--xml=%s" % filename,
+                "--no-head",
+                "--threads",
+                "1"
             ]
 
+            # Add the user args
+            args.extend(user_args)
+
             if info.has_url_params:
-                if self.run_xsser(info.hostname, info.url, xsser_script, args):
-                    results.extend(self.parse_xsser_result(info, filename))
+
+                # Get payload for config injection point
+                args.extend([
+                    "-u",
+                    "%s://%s" % (info.parsed_url.scheme, info.parsed_url.host),
+                ])
+
+                # When we want to try GET parameters, we must pass to xsser one by one.
+                for param, value in info.parsed_url.query_params.iteritems():
+
+                    # Not evaluate web server params
+                    if param in WEB_SERVERS_VARS:
+                        continue
+
+                    # Prepare and reorder params
+                    fixed_params = "&".join(["%s=%s" % (x, y) for x, y in info.parsed_url.query_params.iteritems() if x != param])
+
+                    # Add param to text + fixed params
+                    if fixed_params: # -> empty fixed params
+                        params = "%s?%s&%s=" % (info.parsed_url.path, fixed_params, param)
+                    else:
+                        params = "%s?%s=" % (info.parsed_url.path, param)
+
+                    # Prepary args for xsser
+                    args.extend([
+                        "-g",
+                        params
+                    ])
+
+                    # Run xsser
+                    if self.run_xsser(info.hostname, info.url, args):
+                        results.extend(self.parse_xsser_result(info, filename))
 
             if info.has_post_params:
                 args.extend([
+                    "-u",
+                    info.url,
                     "-p",
                     "&".join(
-                        [ "%s=%s" % (k, v)
-                          for k, v in info.post_params.iteritems() ]
+                        ["%s=%s" % (k, v)
+                            for k, v in info.post_params.iteritems() if k not in WEB_SERVERS_VARS]
                     ),
                 ])
-                if self.run_xsser(info.hostname, info.url, xsser_script, args):
+                if self.run_xsser(info.hostname, info.url, args):
                     results.extend(self.parse_xsser_result(info, filename))
 
         if results:
@@ -106,7 +146,7 @@ class XSSerPlugin(TestingPlugin):
 
 
     #--------------------------------------------------------------------------
-    def run_xsser(self, hostname, url, command, args):
+    def run_xsser(self, hostname, url, args):
         """
         Run XSSer against the given target.
 
@@ -128,6 +168,8 @@ class XSSerPlugin(TestingPlugin):
 
         with ConnectionSlot(hostname):
             t1 = time()
+            # Find one of the paths to the binary
+            command = find_binary_in_path("xsser.py")[0]
             code = run_external_tool(command, args, callback=Logger.log_verbose)
             t2 = time()
 
@@ -219,15 +261,3 @@ class XSSerPlugin(TestingPlugin):
             Logger.log_error_more_verbose(tb)
 
         return result
-
-
-    #--------------------------------------------------------------------------
-    def get_xsser(self):
-        """
-        :return: Path to the XSSer script.
-        :rtype: str
-        """
-        xsser = join(get_tools_folder(), "xsser", "xsser.py")
-        if not isfile(xsser):
-            xsser = find_binary_in_path("xsser")
-        return xsser
