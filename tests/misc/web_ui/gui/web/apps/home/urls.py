f5c9716a9941ece551fb52edcb0d4728192286eb
Mario Vilas <mvilas@gmail.com>
2014-01-10 14:55:26 +0100

    Removing old stuff.

--- tests/misc/web_ui/gui/web/apps/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-                       url(r'index.html$', 'apps.home.views.home', name="view_home_home"),
-                       url(r'login.html$', 'apps.home.views.login', name="view_home_login"),
-                       url(r'^$', 'apps.home.views.master', name="view_home_login"),
-)
